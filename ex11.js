7bdb6a6e72c8c469514d575583be940beac4a4f6
Kazi <kaoru.kajihara.mail@gmail.com>
2026-05-28 18:25:54 -0700

    exercise 11 completed

--- ex11.js
+++ ex11.js
@@ -28,45 +28,54 @@ Implement the functions one by one. The example inputs and outputs below will he
 const PI = 3.14159;
 
 const sphereVolume = function (radius) {
-  // Code here!
+  return (4 / 3) * PI * radius * radius * radius;
 };
 
 console.log(4186 < sphereVolume(10) && sphereVolume(10) < 4189); //true
 
 const coneVolume = function (radius, height) {
-  // And here!
+  return (PI * radius * radius * height) / 3;
 };
 
 console.log(45 < coneVolume(3, 5) && coneVolume(3, 5) < 49); //true
 
 const prismVolume = function (height, width, depth) {
-  // Probably here too!
+  return width * depth * height;
 };
 
 console.log(prismVolume(3, 4, 5) === 60); //true
 
 const totalVolume = function (solids) {
-  // Code here? Yup!
+  return solids.reduce((total, shape) => {
+    switch (shape.type) {
+      case 'sphere':
+        return total + sphereVolume(shape.radius);
+      case 'cone':
+        return total + coneVolume(shape.radius, shape.height);
+      case 'prism':
+        return total + prismVolume(shape.height, shape.width, shape.depth);
+      default:
+    }
+  }, 0);
 };
 
 const largeSphere = {
-  type: "sphere",
+  type: 'sphere',
   radius: 40,
 };
 
 const smallSphere = {
-  type: "sphere",
+  type: 'sphere',
   radius: 10,
 };
 
 const cone = {
-  type: "cone",
+  type: 'cone',
   radius: 3,
   height: 5,
 };
 
 const duck = [largeSphere, smallSphere, cone];
-
 console.log(272000 < totalVolume(duck) && totalVolume(duck) < 275000); //true
 
 module.exports = {
