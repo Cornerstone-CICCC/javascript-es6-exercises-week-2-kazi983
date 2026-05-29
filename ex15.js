f57a271b7d46f50ddb30b62741f15fe8c152147b
Kazi <kaoru.kajihara.mail@gmail.com>
2026-05-28 19:29:44 -0700

    exercise 15 completed

--- ex15.js
+++ ex15.js
@@ -9,24 +9,30 @@ Create a function named organizeInstructors that will receive an array of instru
 */
 
 const organizeInstructors = function (instructors) {
-  // Put your solution here
+  return instructors.reduce((organizedInstructors, instructor) => {
+    if (!organizedInstructors[instructor.course]) {
+      organizedInstructors[instructor.course] = [];
+    }
+    organizedInstructors[instructor.course].push(instructor.name);
+    return organizedInstructors;
+  }, {});
 };
 
 console.log(
   organizeInstructors([
-    { name: "Samuel", course: "iOS" },
-    { name: "Victoria", course: "Web" },
-    { name: "Karim", course: "Web" },
-    { name: "Donald", course: "Web" },
-  ])
+    { name: 'Samuel', course: 'iOS' },
+    { name: 'Victoria', course: 'Web' },
+    { name: 'Karim', course: 'Web' },
+    { name: 'Donald', course: 'Web' },
+  ]),
 ); // { iOS: ["Samuel"], Web: ["Victoria", "Karim", "Donald"]}
 console.log(
   organizeInstructors([
-    { name: "Brendan", course: "Blockchain" },
-    { name: "David", course: "Web" },
-    { name: "Martha", course: "iOS" },
-    { name: "Carlos", course: "Web" },
-  ])
+    { name: 'Brendan', course: 'Blockchain' },
+    { name: 'David', course: 'Web' },
+    { name: 'Martha', course: 'iOS' },
+    { name: 'Carlos', course: 'Web' },
+  ]),
 ); //{ Blockchain: ["Brendan"], Web: ["David", "Carlos"], iOS: ["Martha"]}
 
 module.exports = organizeInstructors;
