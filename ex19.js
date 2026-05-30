39a43d3a00149d0db76d12bbe144b842b0695bd1
Kazi <kaoru.kajihara.mail@gmail.com>
2026-05-29 17:57:28 -0700

    exercise 19 completed

--- ex19.js
+++ ex19.js
@@ -66,3 +66,84 @@ Instruction
 Create a function generateBoard which will return a nested array representing the board, containing the location of two queens.
 Create a function called queenThreat that will indicate whether or not the two queens are positioned so that they attack each other.
 */
+
+/**
+ * Generate an 8x8 chessboard with two queens placed on it.
+ *
+ * @param {number[]} queen1 - The position of the first queen in the format [row, column].
+ * @param {number[]} queen2 - The position of the second queen in the format [row, column].
+ * @returns {number[][]} An 8x8 chessboard where queens are represented by 1.
+ */
+const generateBoard = (queen1, queen2) => {
+  const board = [];
+  for (let i = 0; i < 8; i++) {
+    board.push([0, 0, 0, 0, 0, 0, 0, 0]);
+  }
+  board[queen1[0]][queen1[1]] = 1;
+  board[queen2[0]][queen2[1]] = 1;
+
+  return board;
+};
+
+/**
+ * Check whether two queens can attack each other.
+ *
+ * @param {number[][]} board - An 8x8 chessboard where queens are represented by 1.
+ * @returns {boolean} True if the queens are on the same row, column, or diagonal.
+ */
+const queenThreat = (board) => {
+  /**
+   * Detect queens positions on the board.
+   *
+   * @param {number[][]} board - An 8x8 chessboard where queens are represented by 1.
+   * @returns {number[][]} An array of queen positions in the format [row, column].
+   */
+  const detectQueen = (board) => {
+    let indexQueens = [];
+
+    board.forEach((row, index) => {
+      if (row.includes(1)) {
+        indexQueens.push([index, row.indexOf(1)]);
+      }
+    });
+
+    return indexQueens;
+  };
+
+  /**
+   * Check whether two queens can attack each other horizontally or vertically.
+   *
+   * @param {number[][]} queensIndex - Queen positions in the format [row, column].
+   * @returns {boolean} True if the queens are on the same row or column.
+   */
+  const searchOrthogonal = (queensIndex) =>
+    queensIndex[0][0] === queensIndex[1][0] || queensIndex[0][1] === queensIndex[1][1];
+
+  /**
+   * Check whether two queens can attack each other diagonally.
+   *
+   * @param {number[][]} queensIndex - Queen positions in the format [row, column].
+   * @returns {boolean} True if the queens are on the same diagonal.
+   */
+  const searchDiagonal = (
+    queensIndex, // [0,5]-[1,4][2,3]->true [0,5]-[3,2]->false
+  ) =>
+    queensIndex[0][0] + queensIndex[0][1] === queensIndex[1][0] + queensIndex[1][1] ||
+    queensIndex[0][0] - queensIndex[0][1] === queensIndex[1][0] - queensIndex[1][1];
+
+  const queensIndex = detectQueen(board);
+
+  return searchOrthogonal(queensIndex) || searchDiagonal(queensIndex);
+};
+
+// let whiteQueen = [0, 5];
+// let blackQueen = [5, 0];
+// let generatedBoard = generateBoard(whiteQueen, blackQueen);
+// console.log(generatedBoard);
+// console.log(queenThreat(generatedBoard));
+
+let whiteQueen = [0, 0];
+let blackQueen = [5, 7];
+let generatedBoard = generateBoard(whiteQueen, blackQueen);
+console.log(generatedBoard);
+console.log(queenThreat(generatedBoard));
